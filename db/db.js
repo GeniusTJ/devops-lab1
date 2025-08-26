0c1da939ffdcc4484178650d315b4dede034127f
Tilan Jayaweera <githubtilan@gmail.com>
2025-08-27 03:15:37 +0530

    test

--- db/db.js
+++ db/db.js
@@ -2,6 +2,18 @@ const mysql = require("mysql2");
 const dotenv = require("dotenv");
 dotenv.config();
 
+console.log({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+  timezone: "+05:30",
+  dateStrings: ["DATE", "DATETIME", "TIMESTAMP"],
+});
+
 const db = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
